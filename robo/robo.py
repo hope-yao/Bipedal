40551df9161a1ab25ab9833e90b45c61052faf1d
hope-yao <hope-yao@asu.edu>
2016-04-27 20:31:00 -0700

    some changes in *.py, try to walk more than one cycle

--- robo/robo.py
+++ robo/robo.py
@@ -93,38 +93,6 @@ def two_linked_chain(state, t):
     return xd
 
 
-def heel_strike(state):
-    q1 = state[0]
-    q2 = state[2]
-
-    alpha = cos(q1-q2)
-
-    Q_r = np.zeros((2,2))
-    Q_r[1,1] = 0
-    Q_r[1,0] = -ms*a1*(lt+b1) + mt*b2*(ls+a2)
-    Q_r[0,1] = Q_r[1,0]
-    Q_r[0,0] = Q_r[0,1] + (mh*l + 2*mt*(a2+ls) + ms*a1) * l*cos(alpha)
-
-    Q_l = np.zeros((2,2))
-    Q_l[1,1] = ms*(lt+b1)**2 + mt*b2**2
-    Q_l[1,0] = -(ms*(b1+lt) + mt*b2) * l*cos(alpha)
-    Q_l[0,1] = Q_l[1,0] + ms*(b1+lt)**2 + mt*b2**2
-    Q_l[0,0] = Q_l[1,0] + (ms+mt+mh)*l**2 + ms*a1**2 + mt*(a2+ls)**2
-
-    q_r = [state[1],state[3]]
-    rhs = np.dot(Q_r,q_r)
-    sol = np.linalg.solve(Q_l,rhs)
-
-    xd = np.zeros_like(state)
-    xd[0] = state[2]
-    xd[1] = sol[0]
-    xd[2] = state[0]
-    xd[3] = sol[1]
-    xd[4] = state[0]
-    xd[5] = sol[1]
-
-    return xd
-
 def knee_strike(state):
     q1 = state[0]
     q2 = state[2]
@@ -153,18 +121,50 @@ def knee_strike(state):
     sol = np.linalg.solve(Q_l,rhs)
 
     xd = np.zeros_like(state)
-    xd[0] = state[1]
+    xd[0] = state[0]
     xd[1] = sol[0]
-    xd[2] = state[3]
+    xd[2] = state[2]
     xd[3] = sol[1]
-    xd[4] = state[5]
+    xd[4] = state[4]
+    xd[5] = sol[1]  # q3d = q2d, thigh and shank binded
+
+    return xd
+
+def heel_strike(state):
+    q1 = state[0]
+    q2 = state[2]
+
+    alpha = cos(q1-q2)
+
+    Q_r = np.zeros((2,2))
+    Q_r[1,1] = 0
+    Q_r[1,0] = -ms*a1*(lt+b1) + mt*b2*(ls+a2)
+    Q_r[0,1] = Q_r[1,0]
+    Q_r[0,0] = Q_r[0,1] + (mh*l + 2*mt*(a2+ls) + ms*a1) * l*cos(alpha)
+
+    Q_l = np.zeros((2,2))
+    Q_l[1,1] = ms*(lt+b1)**2 + mt*b2**2
+    Q_l[1,0] = -(ms*(b1+lt) + mt*b2) * l*cos(alpha)
+    Q_l[0,1] = Q_l[1,0] + ms*(b1+lt)**2 + mt*b2**2
+    Q_l[0,0] = Q_l[1,0] + (ms+mt+mh)*l**2 + ms*a1**2 + mt*(a2+ls)**2
+
+    q_r = [state[1],state[3]]
+    rhs = np.dot(Q_r,q_r)
+    sol = np.linalg.solve(Q_l,rhs)
+
+    xd = np.zeros_like(state)
+    xd[0] = state[0]
+    xd[1] = sol[0]
+    xd[2] = state[2]
+    xd[3] = sol[1]
+    xd[4] = state[4]
     xd[5] = sol[1]  # q3d = q2d, thigh and shank binded
 
     return xd
 
 
-def step_cycle(state,_time):
-    print('initial position')
+def step_cycle(state,pos_sf,_time):
+    print('>>>>>>>>>>>>>>>>>>>>>>>initial position<<<<<<<<<<<<<<<<<<<<<<<<<<<')
     print(np.degrees([state[0],state[2],state[4]]))
 
     ttcosd = []
@@ -172,7 +172,7 @@ def step_cycle(state,_time):
     num_time = 2
     dt = _time / num_time
     chain_num=3 # starts with three-chain state
-    while cnt<300:
+    while cnt<300: # usually it takes less than three seconds for a step
         print('cnt: ',cnt)
         t = np.arange(0.0, _time, dt)
         if cnt==0:
@@ -186,7 +186,7 @@ def step_cycle(state,_time):
         if  (np.max(-(state[-num_time:,2]))>np.radians(_alpha/2-_gamma)*0.9)\
         and (chain_num==3) and (np.min(np.abs(state[-num_time:,2]-state[-num_time:,4]))<np.radians(1)):
             tmp = knee_strike(state[-1])
-            # state = np.insert(tmp,[0],state,axis=0)
+            state = np.insert(state, [len(state)], tmp, axis=0)
             chain_num = 2
             print('===============================================knee strike, time: %s' % (cnt*_time))
             print(np.degrees([state[-1,0],state[-1,2],state[-1,4]]))
@@ -194,12 +194,10 @@ def step_cycle(state,_time):
         # heel strike
         if (chain_num==2) and cosd<(3): # in degree
             tmp = heel_strike(state[-1])
-            # state = np.insert(tmp,[0],state,axis=0)
+            state = np.insert(state, [len(state)], tmp, axis=0)
             chain_num = 3
             print('===============================================heel strike, time: %s' % (cnt*_time))
             print(np.degrees([state[-1,0],state[-1,2],state[-1,4]]))
-            # switching nonstance leg and stance leg at this moment
-            # continue
             break
         # three linked chain
         if (chain_num==3):
@@ -232,7 +230,7 @@ def step_cycle(state,_time):
         v2 = [cos(np.radians(_gamma)),-sin(np.radians(_gamma))]
         ab = np.dot(v1, v2)
         print(ab)
-        cosd = [np.degrees(math.acos( 0.999* ab[i] / np.linalg.norm((x_nsf_tmp[i],y_nsf_tmp[i])))) for i in range(num_time)]
+        cosd = [np.degrees(math.acos( 0.999* ab[i] / np.linalg.norm((x_nsf_tmp[i],y_nsf_tmp[i])))) for i in range(num_time)] #don't mater to much for origin or nonstance feet
         # tmp = ab / np.linalg.norm([x_nsf_tmp,y_nsf_tmp])
         # cosd = [np.degrees(math.acos(tmp[0]))]
         ttcosd = ttcosd + (cosd)
@@ -243,14 +241,14 @@ def step_cycle(state,_time):
     q1 = state[:,0]
     q2 = state[:,2]
     q3 = state[:,4]
-    x_h = -l*sin(q1)
-    y_h = l*cos(q1)
+    x_h = pos_sf[0] - l*sin(q1)
+    y_h = pos_sf[1] + l*cos(q1)
     x_nsk = x_h - lt*sin(q2)
     y_nsk = y_h - lt*cos(q2)
     x_nsf = x_nsk - ls*sin(q3)
     y_nsf = y_nsk - ls*cos(q3)
 
-    return x_h,y_h,x_nsk,y_nsk,x_nsf,y_nsf,state[-1,:]
+    return x_h,y_h,x_nsk,y_nsk,x_nsf,y_nsf,state
 
 def init():
     for line in lines:
@@ -263,8 +261,9 @@ def animate(i):
 
     x_bipedal = [0, x_h[i], x_nsk[i], x_nsf[i]]
     y_bipedal = [0, y_h[i], y_nsk[i], y_nsf[i]]
-    # x_bipedal = [0, x_h[-10], x_nsk[-10], x_nsf[-10]]
-    # y_bipedal = [0, y_h[-10], y_nsk[-10], y_nsf[-10]]
+    # k=-1
+    # x_bipedal = [0, x_h[k], x_nsk[k], x_nsf[k]]
+    # y_bipedal = [0, y_h[k], y_nsk[k], y_nsf[k]]
 
     x_slop = [x_slop_up,x_slop_low,-2]
     y_slop = [y_slop_up,y_slop_low,y_slop_low]
@@ -321,11 +320,19 @@ q2d = 0.0
 q3 = q2
 q3d = 0.0
 state = np.radians([q1, q1d, q2, q2d, q3, q3d])
+pos_sf = [0,0]
 
-x_h, y_h, x_nsk, y_nsk, x_nsf, y_nsf,state = step_cycle(state, dt)
-# for i in range(2):
-#     x_h_, y_h_, x_nsk_, y_nsk_, x_nsf_, y_nsf_, state = step_cycle(state, dt)
-#     x_h.append(x_h_)
+x_h, y_h, x_nsk, y_nsk, x_nsf, y_nsf,state = step_cycle(state, pos_sf, dt)
+# for i in range(1):
+#     ini_state = [state[-1, 0],state[-1, 1],state[-1, 2],state[-1, 3],state[-1, 4],state[-1, 5]]
+#     x_h_new, y_h_new, x_nsk_new, y_nsk_new, x_nsf_new, y_nsf_new, state = step_cycle(ini_state, dt)
+#     x_h = np.insert(x_h,[len(x_h)],x_h_new,axis=0)
+#     y_h = np.insert(y_h,[len(y_h)],y_h_new,axis=0)
+#     x_nsk = np.insert(x_nsk,[len(x_nsk)],x_nsk_new,axis=0)
+#     y_nsk = np.insert(y_nsk,[len(y_nsk)],y_nsk_new,axis=0)
+#     x_nsf = np.insert(x_nsf,[len(x_nsf)],x_nsf_new,axis=0)
+#     y_nsf = np.insert(y_nsf,[len(y_nsf)],y_nsf_new,axis=0)
+#     pos_sf = [x_nsf[-1],y_nsf[-1]]
 
 # f = open('out.txt', 'w')
 # f.write(str(x_h))
