d543582cfffb669cfd9c71118e4bc78e8cbc2b0c
hope-yao <hope-yao@asu.edu>
2016-05-04 20:32:06 -0700

    added scikits for event detection, noticed that three link dynamics is not the same as paper shows

--- robo/opt_robo.py
+++ robo/opt_robo.py
@@ -19,29 +19,34 @@ import robo
 
 c_alpha =  1 # defined by angle between two legs, times 20 in degree
 # constrain:
-q1 = 0.1877
-q2 = -0.2884
-q3 = -0.2884
-c_a1 = 0.375
-c_b1 = 0.125
-c_a2 = 0.175
-c_b2 = 0.325
-# q1 = 0.15
-# q2 = -0.25
-# q3 = -0.25
-# c_a1 = 0.3
-# c_b1 = 0.2
-# c_a2 = 0.2
-# c_b2 = 0.3
+# q1 = 0.1877
+# q2 = -0.2884
+# q3 = -0.2884
+# c_a1 = 0.375
+# c_b1 = 0.125
+# c_a2 = 0.175
+# c_b2 = 0.325
+q1 = 0.15
+q2 = -0.25
+q3 = -0.25
+c_a1 = 0.25
+c_b1 = 0.25
+c_a2 = 0.25
+c_b2 = 0.25
 leg_struc = np.array([q1, q2, q3, c_a1, c_b1, c_a2, c_b2])
+# 0.1894 -0.2942 -0.2942 0.375 0.125 0.175 0.352
+global cnt
+cnt = 0
 
 para_file = 'parameters.txt'
 np.savetxt(para_file, [leg_struc], delimiter='  ')
 robo.robo(1)
 
-def obj_fun(x,show_ani):
-    print(">>>>>>>>>>>>>>>>>>>>>>>leg structure<<<<<<<<<<<<<<<<<<<<<<<<<<<")
-    print(x)
+def obj_fun(x,arg):
+    arg[0] = arg[0] + 1
+    show_ani = arg[1]
+    # print(">>>>>>>>>>>>>>>>>>>>>>>leg structure<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+    # print(x)
     para_file = 'parameters.txt'
     np.savetxt(para_file, [x], delimiter='  ')
     robo.robo(show_ani)
@@ -49,7 +54,15 @@ def obj_fun(x,show_ani):
     obj_val = np.loadtxt(out_file, delimiter='  ')
     # minimize negative displacement
     paerto_obj = obj_val[0] * pareto_para[0] - obj_val[1]*pareto_para[1]
-    print('obj value:', obj_val)
+
+    iter_info = [arg[0]] + obj_val.tolist()
+    print('number of simulation:',iter_info[0])
+    print('obj value:', iter_info[1:3])
+    f = open('iteration.txt', 'a')
+    f.write(str(iter_info)[1:-1])
+    f.write('\n')
+    f.close()
+
     # obj with initial para:
     # save for T-SNE
     result_obj = np.loadtxt('out.txt', delimiter='  ')
@@ -64,13 +77,14 @@ def obj_fun(x,show_ani):
 
 
 pareto_para = [[],[]]
-num_pareto_points = 20
+num_pareto_points = 10
 tt = [(i / (num_pareto_points+1)) for i in range(num_pareto_points+1)]
 tt.pop(0)
-tt.pop(0)
+tt = [1]
+print(tt)
 for w in tt:
-    pareto_para[0] = w / num_pareto_points
-    pareto_para[1] = 1 - pareto_para[0]
+    pareto_para[0] = w
+    pareto_para[1] = 1 - w
 
     cons = (
         # total weight in range[0.8,1.0]
@@ -88,8 +102,8 @@ for w in tt:
          'fun': lambda x: np.array([-0.9 + (x[4] + x[5] + x[6] + x[7])]),
          'jac': lambda x: np.array([0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0])}
     )
-    para_bound = [(0.15,0.2),(-0.3,-0.2),(-0.3,-0.2),(0.2,0.4),(0.1,0.2),(0.1,0.2),(0.2,0.4)]
-    res = minimize(obj_fun, leg_struc, args=0, method='L-BFGS-B', jac=None, bounds=para_bound, tol=1e-4, options={ 'maxiter':1000, 'disp': False})
+    para_bound = [(0.15,0.2),(-0.3,-0.2),(-0.3,-0.2),(0.2,0.4),(0.1,0.26),(0.1,0.26),(0.2,0.4)]
+    res = minimize(obj_fun, leg_struc, args=[cnt,0], method='L-BFGS-B', jac=None, bounds=para_bound, tol=1e-4, options={ 'maxiter':5000, 'disp': False})
 
     # f = open('bestresult.txt', 'a')
     # f.write(str(result)[1:-1])
@@ -101,7 +115,7 @@ for w in tt:
 
     print('optimization result:')
     print(res)
-    # robo.robo(1)
+    robo.robo(1)
 
 
 # fmin_l_bfgs_b(obj_fun, leg_struc, approx_grad=True, bounds=None, m=10, factr=10000000.0, pgtol=1e-05, epsilon=1e-08, iprint=-1, maxfun=15000, maxiter=15000, disp=None, callback=None, maxls=20)
--- robo/robo.py
+++ robo/robo.py
@@ -3,6 +3,110 @@ def lineno():
     """Returns the current line number in our program."""
     return inspect.currentframe().f_back.f_lineno
 
+'''
+def three_linked_chain(t, x, xdot):
+    state = x
+    q1 = state[0]
+    q1d = state[1]
+    q2 = state[2]
+    q2d = state[3]
+    q3 = state[4]
+    q3d = state[5]
+
+    H = np.zeros((3,3))
+    H[0,0] = ms*a1**2 + mt*(ls+a2)**2 + (mh+ms+mt)*l**2
+    H[0,1] = -(mt*b2+ms*lt) * l * cos(q2-q1)
+    H[0,2] = -ms*b1*l * cos(q3-q1)
+    H[1,0] = H[0,1]
+    H[1,1] = mt*b2**2 + ms*lt**2
+    H[1,2] = ms*lt*b1 * cos(q3-q2)
+    H[2,0] = H[0,2]
+    H[2,1] = H[1,2]
+    H[2,2] = ms*b1**2
+
+    B = np.zeros((3,3))
+    h122 = -(mt*b2+ms*lt) * l * sin(q1-q2)
+    h133 = -ms*b1*l * sin(q1-q3)
+    h211 = -h122
+    h233 = ms*lt*b1 * sin(q3-q2)
+    h311 = -h133
+    h322 = -h233
+    B[0,0] = 0
+    B[0,1] = h122 * q2d
+    B[0,2] = h133 * q3d
+    B[1,0] = h211 * q1d
+    B[1,1] = 0
+    B[1,2] = h233 * q3d
+    B[2,0] = h311 * q1d
+    B[2,1] = h322 * q2d
+    B[2,2] = 0
+
+    G = np.zeros(3)
+    G[0] = -(ms*a1 + mt*(ls+a2) + (mh+ms+mt)*l) * g*sin(q1)
+    G[1] = (mt*b2 + ms*lt) * g*sin(q2)
+    G[2] = ms*b1*g * sin(q3)
+
+    qd = [q1d,q2d,q3d]
+    rhs = -np.dot(B,qd) - G
+    try:
+        sol = np.linalg.solve(H,rhs)
+    except:
+        print(H)
+        sys.exit("wrong in solving three linked dynamics!")
+
+    xdot[0] =  q1d
+    xdot[1] =  sol[0]
+    xdot[2] =  q2d
+    xdot[3] =  sol[1]
+    xdot[4] =  q3d
+    xdot[5] =  sol[2]
+
+def root_ks(t, y, out):
+    """ root function to check the object reached height Y1 """
+    out[0] = y[2] - y[4]
+    return 0
+
+def rhseqn(t, x, xdot):
+    """ we create rhs equations for the problem"""
+    xdot[0] = x[1]
+    xdot[1] = - 4 * x[0]
+
+def root_fn(t, y, out):
+    """ root function to check the object reached height Y1 """
+    Y1 = 0.0
+    out[0] = Y1 + y[0]
+    return 0
+def print_results(experiment_no, result, require_no_roots=False):
+    ts, ys = result.values.t, result.values.y
+    # Print computed values at tspan
+    print('\n Experiment number: ', experiment_no)
+    print('--------------------------------------')
+    print('    t             Y               v')
+    print('--------------------------------------')
+
+    for (t, y, v) in zip(ts, ys[:, 0], ys[:, 1]):
+        print('{:6.1f} {:15.4f} {:15.4f}'.format(t, y, v))
+
+
+    t_roots, y_roots = result.roots.t, result.roots.y
+    if not require_no_roots:
+        # Print interruption points
+        print('\n t_roots     y_roots        v_roots')
+        print('--------------------------------------')
+        if (t_roots is None) and (y_roots is None):
+            print('{!s:6} {!s:15} {!s:15}'.format(t_roots, y_roots, y_roots))
+        elif (t_roots is not None) and (y_roots is not None):
+            if np.isscalar(t_roots):
+                print('{:6.1f} {:15.4f} {:15.2f}'.format(t_roots, y_roots[0], y_roots[1]))
+            else:
+                for (t, y, v) in zip(t_roots, y_roots[:, 0], y_roots[:, 1]):
+                    print('{:6.6f} {:15.4f} {:15.4f}'.format(t, y, v))
+        else:
+            print('Error: one of (t_roots, y_roots) is None while the other not.')
+    else:
+        print('Computation failed.')
+
+'''
 
 def three_linked_chain(state, t):
     q1 = state[0]
@@ -51,16 +155,9 @@ def three_linked_chain(state, t):
         sol = np.linalg.solve(H,rhs)
     except:
         print(H)
-    xd = np.zeros_like(state)
-    xd[0] = q1d
-    xd[1] = sol[0]
-    xd[2] = q2d
-    xd[3] = sol[1]
-    xd[4] = q3d
-    xd[5] = sol[2]
+        sys.exit("wrong in solving three linked dynamics!")
 
-    return xd
-
+    return [ q1d, sol[0], q2d, sol[1], q3d, sol[2] ]
 
 
 def two_linked_chain(state, t):
@@ -145,21 +242,23 @@ def heel_strike(state):
     alpha = cos(q1-q2)
 
     Q_r = np.zeros((2,2))
-    Q_r[1,1] = 0
-    Q_r[1,0] = -ms*a1*(lt+b1) + mt*b2*(ls+a2)
-    Q_r[0,1] = Q_r[1,0]
+    Q_r[0,1] = -ms*a1*(lt+b1) + mt*b2*(ls+a2)
     Q_r[0,0] = Q_r[0,1] + (mh*l + 2*mt*(a2+ls) + ms*a1) * l*cos(alpha)
+    Q_r[1,1] = 0
+    Q_r[1,0] = Q_r[0,1]
 
     Q_l = np.zeros((2,2))
-    Q_l[1,1] = ms*(lt+b1)**2 + mt*b2**2
     Q_l[1,0] = -(ms*(b1+lt) + mt*b2) * l*cos(alpha)
-    Q_l[0,1] = Q_l[1,0] + ms*(b1+lt)**2 + mt*b2**2
     Q_l[0,0] = Q_l[1,0] + (ms+mt+mh)*l**2 + ms*a1**2 + mt*(a2+ls)**2
+    Q_l[0,1] = Q_l[1,0] + ms*(b1+lt)**2 + mt*b2**2
+    Q_l[1,1] = ms*(lt+b1)**2 + mt*b2**2
 
     q_r = [state[1],state[3]]
     rhs = np.dot(Q_r,q_r)
     sol = np.linalg.solve(Q_l,rhs)
 
+    A = np.linalg.inv(Q_l)
+    B = np.dot(A,Q_r)
     xd = np.zeros_like(state)
     xd[0] = state[0]
     xd[2] = state[2]
@@ -171,8 +270,9 @@ def heel_strike(state):
     return xd
 
 
-def step_cycle(state,pos_sf,_time):
-    print('start state: ', state)
+
+def step_cycle(state,pos_sf,_time,step_out=0):
+    # print('start state: ', state)
 
     ttcosd = []
     cnt = 0
@@ -180,6 +280,8 @@ def step_cycle(state,pos_sf,_time):
     dt = _time / num_time
     chain_num=3 # starts with three-chain state
     success = 1
+
+
     while True:
         try:
             # print('cnt: ',cnt)
@@ -192,62 +294,83 @@ def step_cycle(state,pos_sf,_time):
                     print(state)
                     print(lineno())
                     sys.exit("wrong in this step!")
-                # print('three linked chain, time: %s <<<' % (cnt*_time))
-                # print(np.degrees([state[-1,0],state[-1,2],state[-1,4]]))
+                if step_out:
+                    print('three linked chain, time: %s <<<' % (cnt * _time))
+                    print(state[-1])
                 cnt += 1
                 continue
             # knee strike
-            if  ((state[-num_time:,2])>(state[-num_time:,0])).any()\
-            and (chain_num==3) and (np.min(np.abs(state[-num_time:,2]-state[-num_time:,4]))<np.radians(1)):
+            if  ((state[-num_time:,2])>(state[-num_time:,0])).any() and cnt>0.1/_time\
+            and (chain_num==3) and (np.min(state[-num_time:,2]-state[-num_time:,4])<np.radians(0.001)):
                 tmp = knee_strike(state[-1])
                 state = np.insert(state, [len(state)], tmp, axis=0)
                 chain_num = 2
-                print('===============================================knee strike, time: %s' % (cnt*_time))
-                print(state[-1])
+                if step_out:
+                    print('===============================================knee strike, time: %s' % (cnt*_time))
+                    print(state[-1])
                 continue
             # heel strike
             if (chain_num==2) and (dist<0.005): # in degree
                 tmp = heel_strike(state[-1])
                 state = np.insert(state, [len(state)], tmp, axis=0)
                 chain_num = 3
-                print('===============================================heel strike, time: %s' % (cnt*_time))
-                print(state[-1])
-                print('end state: ',state[-1,:])
+                if step_out:
+                    print('===============================================heel strike, time: %s' % (cnt*_time))
+                    print(state[-1])
+                    # print('end state: ',state[-1,:])
+
+                # update initial condition
+                q1 = (state[-1, 2] + state[-1, 4]) / 2
+                q1d = (state[-1, 3] + state[-1, 5]) / 2
+                q2 = state[-1, 0]
+                q2d = state[-1, 1]
+                q3 = state[-1, 0]
+                q3d = state[-1, 1]
+                tmp = [q1, state[0, 1], q2, state[0, 3], q3,state[0, 5]]  ##############################ATTENTION HERE!
+                tmp = [q1, q1d, q2, q2d, q3, q3d]  ##############################ATTENTION HERE!
+                state = np.insert(state, [len(state)], tmp, axis=0)
+                if step_out:
+                    print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>new initial')
+                    print(state[-1])
+
                 break
             # three linked chain
             if (chain_num==3):
                 try:
                     tmp = integrate.odeint(three_linked_chain, state[-1], t)
                     state = np.insert(tmp[1:len(tmp)+1],[0],state,axis=0)
-                    # print('three linked chain, time: %s <<<' % (cnt*_time))
-                    # print(state[-1])
+                    if step_out:
+                        print('three linked chain, time: %s <<<' % (cnt*_time))
+                        print(state[-1])
                     chain_num = 3
                     cnt += 1
                 except:
                     print(state)
                     print(lineno())
-                    sys.exit("wrong in this step!")
+                    break
+                    # sys.exit("wrong in this step!")
             # two linked chain
             if (chain_num==2):
                 try:
                     tmp = integrate.odeint(two_linked_chain, state[-1], t)
                     state = np.insert(tmp[1:len(tmp)+1],[0],state,axis=0)
-                    # print('two linked chain, time: %s <<<' % (cnt*_time))
-                    # print(state[-1])
+                    if step_out:
+                        print('two linked chain, time: %s <<<' % (cnt*_time))
+                        print(state[-1])
                     chain_num = 2
                     cnt += 1
                 except:
                     print(state)
                     print(lineno())
-                    sys.exit("wrong in this step!")
+                    break
+                    # sys.exit("wrong in this step!")
         except:
             print("this is not a good step!")
             success = 0
-            break
 
-        q1 = tmp[:,0]
-        q2 = tmp[:,2]
-        q3 = tmp[:,4]
+        q1 = tmp[0:-1,0]
+        q2 = tmp[0:-1,2]
+        q3 = tmp[0:-1,4]
         x_h_tmp = -l*sin(q1)
         y_h_tmp = l*cos(q1)
         x_nsk_tmp = x_h_tmp + lt*sin(q2)
@@ -269,9 +392,9 @@ def step_cycle(state,pos_sf,_time):
             success = 0
             break
 
-    q1 = state[:,0]
-    q2 = state[:,2]
-    q3 = state[:,4]
+    q1 = state[0:-1,0]
+    q2 = state[0:-1,2]
+    q3 = state[0:-1,4]
     x_h = pos_sf[0] - l*sin(q1)
     y_h = pos_sf[1] + l*cos(q1)
     x_nsk = x_h + lt*sin(q2)
@@ -309,8 +432,6 @@ def animate(i):
 
     time_text.set_text('time = %.3fs' % (i*dt))
 
-    # return tuple(lines) + (time_text,)
-
     try:
         orbit_q1 = orbit[0:i, 0]
         orbit_q1d = orbit[0:i, 1]
@@ -332,13 +453,6 @@ def animate(i):
     return tuple(lines_obt) + tuple(lines) + (time_text_obt,) + (time_text,)
 
 
-def init_obt():
-    for line in lines_obt:
-        line.set_data([],[])
-    time_text_obt.set_text('')
-    return tuple(lines_obt) + (time_text_obt,)
-
-
 def robo(show_ani):
     # Passive Dynamic Walking for bipedal robot
     # % reset
@@ -352,9 +466,9 @@ def robo(show_ani):
     q1 = last[0]
     q2 = last[1]
     q3 = last[2]
-    c_mh = 0.47
-    c_mt = 0.47
-    c_ms = 0.06
+    c_mh = 0.5
+    c_mt = 0.5
+    c_ms = 0.05
     c_a1 = last[3]
     c_b1 = last[4]
     c_a2 = last[5]
@@ -379,25 +493,79 @@ def robo(show_ani):
     dt = 0.001 # time step of simulation
     step_idx = 1
     step_tt = 1
+    step_out = 0  # show state information at every time step of the first step cycle
     if show_ani:
-        step_tt = 3
+        step_tt = 6
+        step_out = 1  # show state information at every time step of the first step cycle
 
     # slop of terran
     global _gamma
-    # _gamma = 0.0504
-    _gamma = 0.0904
+    _gamma = 0.0504
+    # _gamma = 0.0904
 
 
-    # state = [0.1877, -1.1014, -0.2884, -0.0399, -0.2884, -0.0399]
-    state = [q1, -1.1014, q2, -0.0399, q3, -0.0399]
+    state = [0.1877, -1.1014, -0.2884, -0.0399, -0.2884, -0.0399]
+    # state = [q1, -1.1014, q2, -0.0399, q3, -0.0399]
+    print('init:',state)
     pos_sf = [0,0]
 
 
     global x_sf,y_sf,x_h,y_h,x_nsk,y_nsk,x_nsf,y_nsf
     global x_sk, y_sk
 
+    t = np.arange(0.0, 1.5, 0.0001)
+    # state = integrate.odeint(three_linked_chain, state, t)
+
+######
+    '''
+    from scikits.odes import ode
+
+    # solver = ode('cvode', three_linked_chain, old_api=False)
+    # solution = solver.solve(t, state)
+
+    # solver = ode('cvode', rhseqn, old_api=False)
+    # initx = [1, 0.1]
+    # solution = solver.solve([0., 1., 2.], initx)
+    # for t, u in zip(solution.values.t, solution.values.y):
+    #     print('{0:>4.0f} {1:15.6g} '.format(t, u[0]))
+
+    solver = ode('cvode', rhseqn, nr_rootfns=1, rootfn=root_fn, old_api=False)
+    t_end2 = 100.0  # Time of free fall for experiments 3,4
+    tspan = np.arange(0, t_end2 + 1, 1.0, np.float)
+    y0 = [1, 0.1]
+    # print_results(1, solver.solve(tspan, y0))
+
+    solver = ode('cvode', three_linked_chain, nr_rootfns=1, rootfn=root_ks, old_api=False)
+    tspan = t
+    y0 = state
+    result = solver.solve(tspan, y0)
+    print(result.roots.t)
+    print(result.roots.y)
+########
+
+    # knee strike
+    for i in range(len(t)):
+        if ((state[i, 2]) > (state[i, 0])) and (state[i, 2] - state[i, 4]) < 0.001:
+            tt = i
+            break
+    state = state[0:tt, :]
+
+    q1 = state[0:-1, 0]
+    q2 = state[0:-1, 2]
+    q3 = state[0:-1, 4]
+    x_h = pos_sf[0] - l * sin(q1)
+    y_h = pos_sf[1] + l * cos(q1)
+    x_nsk = x_h + lt * sin(q2)
+    y_nsk = y_h - lt * cos(q2)
+    x_nsf = x_nsk + ls * sin(q3)
+    y_nsf = y_nsk - ls * cos(q3)
+    show_ani = 1
+    x_sf = np.zeros_like(x_h)
+    y_sf = np.zeros_like(x_h)
+    '''
+
     # start walking....
-    x_h, y_h, x_nsk, y_nsk, x_nsf, y_nsf,state, success = step_cycle(state, pos_sf, dt)
+    x_h, y_h, x_nsk, y_nsk, x_nsf, y_nsf,state, success = step_cycle(state, pos_sf, dt, step_out)
     step_time = len(x_h)
     x_sf = np.zeros_like(x_h)
     y_sf = np.zeros_like(x_h)
@@ -406,16 +574,8 @@ def robo(show_ani):
     if success==0:
         output = [[10000, -10000]]
         np.savetxt('out.txt',output, delimiter='  ')
-        return
+        # return
 
-    # update initial condition
-    q1 = (state[-1, 2] + state[-1, 4]) / 2
-    q1d = (state[-1, 3] + state[-1, 5]) / 2
-    q2 = state[-1, 0]
-    q2d = state[-1, 1]
-    q3 = state[-1, 0]
-    q3d = state[-1, 1]
-    state[-1] = [q1, state[0,1], q2, state[0,3], q3, state[0,5]]   ##############################ATTENTION HERE!
     ini_state = state[-1]
     # update location
     pos_sf = [x_nsf[-1], y_nsf[-1]]
@@ -449,14 +609,7 @@ def robo(show_ani):
         y_nsf = np.insert(y_nsf,[len(y_nsf)],y_nsf_new,axis=0)
         y_nsf = np.insert(y_nsf,[len(y_nsf)],y_nsf_new,axis=0)
         state = np.insert(state,[len(state)],state_new,axis=0)
-        # update initial condition
-        q1 = (state[-1, 2] + state[-1, 4]) / 2
-        q1d = (state[-1, 3] + state[-1, 5]) / 2
-        q2 = state[-1, 0]
-        q2d = state[-1, 1]
-        q3 = state[-1, 0]
-        q3d = state[-1, 1]
-        state[-1] = [q1, state[0, 1], q2, state[0, 3], q3, state[0, 5]]  ##############################ATTENTION HERE!
+
         ini_state = state[-1]
         # update location
         pos_sf = [x_nsf[-1],y_nsf[-1]]
@@ -481,6 +634,10 @@ def robo(show_ani):
         ymax = max([max(orbit[:, 1]), max(orbit[:, 3]), max(orbit[:, 5])])
         xmin = min([min(orbit[:, 0]), min(orbit[:, 2]), min(orbit[:, 4])])
         xmax = max([max(orbit[:, 0]), max(orbit[:, 2]), max(orbit[:, 4])])
+        # ymin = -2.5
+        # ymax = 2.5
+        # xmin = -0.3
+        # xmax = 0.4
         ax1.set_xlim([xmin, xmax])
         ax1.set_ylim([ymin, ymax])
         ax1.grid(True)
@@ -513,10 +670,12 @@ def robo(show_ani):
 
 
         ax2.set_xlim([-2, 2])
-        ax2.set_ylim([-2, 2])
+        ax2.set_ylim([-0.5, 2])
         ax2.grid(True)
-        ax1.set_xlabel('Longitude')
-        ax1.set_ylabel('Latitude')
+        ax1.set_xlabel('q')
+        ax1.set_ylabel('dq')
+        ax2.set_xlabel('Longitude')
+        ax2.set_ylabel('Latitude')
 
         global lines, time_text
         lines = []
@@ -528,16 +687,16 @@ def robo(show_ani):
         time_text = ax2.text(0.05, 0.9, '', transform=ax2.transAxes)
 
         ani = animation.FuncAnimation(fig, animate, np.arange(1, len(x_h)),
-                                      interval=100 * dt, blit=True, init_func=init)
+                                      interval=1, blit=True, init_func=init)
 
         plt.show()
         print('end animation...')
         # Set up formatting for the movie files
-        print('saving animation...')
-        Writer = animation.writers['ffmpeg']
-        writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
-        ani.save('PDW.mp4', writer=writer)
-        print('animation saved')
+        # print('saving animation...')
+        # Writer = animation.writers['ffmpeg']
+        # writer = Writer(fps=100, metadata=dict(artist='Me'), bitrate=1800)
+        # ani.save('PDW.mp4', writer=writer)
+        # print('animation saved')
 
 from numpy import sin, cos
 import numpy as np
